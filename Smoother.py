d4859d9399d978c95bb82b97bb50740932f24d20
Patrick King <pkk4hu@virginia.edu>
2018-05-29 16:09:56 -0700

    Smoother Update
    Merging changes to Smoother class, described in header.

--- Smoother.py
+++ Smoother.py
@@ -1,6 +1,13 @@
-#********************************Smoother.py***********************************#
+#*********************************Smoother.py**********************************#
 #
 # Author: Patrick King, Date: 02/06/18
+#
+# Update (PKK) 04/17/18: Ensured compatibility with Observer and Observable
+# update. Made Nyquist public. Changed SmoothPolarizationFraction to accomodate
+# the more general dust polarization efficiency physics. Added writing
+# capability.
+#
+# Update (PKK) 04/24/18: Bugfixes related to mask operations. Passed testing.
 #
 #******************************************************************************#
 
@@ -13,18 +20,27 @@ import functools
 
 class Smoother(object):
 
+    # Constructor.
     def __init__(self, args):
-        self.fwhm   = args[0]
-        self.N      = args[1]
-        self.boxlen = args[2]
-        self.order  = args[3]
+        self.fwhm    = args[0]
+        self.N       = args[1]
+        self.boxlen  = args[2]
+        self.order   = args[3]
         self.sigma   = self.fwhm/(sqrt(8.0*log(2)))
         self.hwhm    = self.fwhm/2.0
         self.posres  = max(int(self.N*self.sigma/(self.boxlen)), 1)
         self.poshwhm = max( int(self.N*self.hwhm/(self.boxlen)), 1)
-        self.nbn     = int((self.N/self.posres))
+        self.Writer  = Observer([None, self.N, self.boxlen, './'])
 
-    def __Nyquist(self, O):
+    def ChangeOptLabel(self, new_optlabel):
+        Writer.ChangeOptLabel(new_optlabel)
+        return
+
+    def     ChangePath(self, new_path):
+        Writer.ChangePath(new_path)
+        return
+
+    def Nyquist(self, O):
         img       = O.data
         inc       = int(self.posres)
         h         = int(self.posres/2)
@@ -35,49 +51,52 @@ class Smoother(object):
         imgds     = img.compressed()
         O.nyquist = imgds
         return
-        
+
     def Smooth(self, O):
         dn  = filters.gaussian_filter(O.data,self.posres,self.order,mode='wrap')
-        olst = [dn,O.N,O.norm,O.lname,O.sname,O.units,O.colmap,O.ax]
-        olst.append(self.poshwhm)
-        olst.append(self.nbn)
+        dn = np.ma.masked_array(dn, O.data.mask)
+        olst = [dn,
+                O.N,
+                O.norm,
+                O.lname,
+                O.sname,
+                O.units,
+                O.colmap,
+                O.axes,
+                O.rotation,
+                self.poshwhm]
         nobs = Observable(olst)
-        self.__Nyquist(nobs)
+        self.Nyquist(nobs)
         # Keep bounds the same as simulation resolution, for consistency.
         nobs.bounds = O.bounds
-        # Keep bins the same as simulation resolution, but update the binnum.
-        if nobs.norm == 'log':
-            bmin      = np.floor(np.log10(np.min(O.data)))
-            bmax      =  np.ceil(np.log10(np.max(O.data)))
-            nobs.bins = np.logspace(bmin,bmax,nobs.binnum)
-        elif nobs.norm == 'symlog':
-            bmin = np.round(np.log10(np.min(np.absolute(O.data))))
-            bmax = np.round(np.log10(np.max(np.absolute(O.data))))
-            binh        = int((nobs.binnum - 1)/2)
-            neg         = -np.logspace(bmax,bmin,binh)
-            zero        = np.array([0.0])
-            pos         = np.logspace(bmin,bmax,binh)
-            nobs.bins   = np.concatenate((neg,zero,pos)).astype(np.double)
-        else:
-            b = max(np.abs(np.min(O.data)),np.max(O.data))
-            if not np.any(O.data < 0.0):
-                nobs.bins = np.linspace(0.0,b,nobs.binnum)
-            else:
-                nobs.bins = np.linspace(-b,b,nobs.binnum)
+        self.Writer.WriteObservable(nobs)
         return nobs
-
-    def BeamSample(self, O):
-        self.__Nyquist(O)
 
     def SmoothGradient(self, O):
         Nab = Nabla([self.fwhm,self.N,self.boxlen])
         G   = Nab.ComputeGradient(O)
-        self.__Nyquist(G)
+        self.Nyquist(G)
         return G
 
     def SmoothAngleGradient(self, Q, U):
         Nab = Nabla([self.fwhm,self.N,self.boxlen])
         S   = Nab.ComputeAngleGradient(Q,U)
-        self.__Nyquist(S)
+        self.Nyquist(S)
         return S
 
+    def SmoothPolarizationFraction(self, Q, U, I):
+        pdata = np.sqrt(Q.data**2 + U.data**2)/I.data
+        pdata = np.ma.masked_array(pdata, I.data.mask)
+        p = Observable([pdata,
+                        Q.N,
+                        'log',
+                        'Polarization Fraction',
+                        '$p$',
+                        'None',
+                        'plasma',
+                        Q.axes,
+                        Q.rotation,
+                        Q.beam])
+        self.Nyquist(p)
+        self.Writer.WriteObservable(p)
+        return p
